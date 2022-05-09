9d41ca653ebd58e50b384f0ab46dca4de467f33b
alperen-kilic <alp.kilic.97@gmail.com>
2022-05-09 16:06:20 +0300

    finalized design

--- components/Center.js
+++ components/Center.js
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
@@ -42,9 +42,9 @@ function Center() {
 
   console.log(playlist);
   return (
-    <div className="flex-grow">
+    <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut}>
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
@@ -56,7 +56,7 @@ function Center() {
       </header>
 
       <section
-        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
       >
         <img className="h-44 w-44 shadow-2xl" src={playlist?.images?.[0]?.url} alt="" />
         <div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
+import { getSession } from "next-auth/react";
 import Center from "../components/Center";
+import Player from "../components/Player";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
@@ -10,7 +10,17 @@ export default function Home() {
         <Sidebar />
         <Center />
       </main>
-      <div>{/* Player */}</div>
+      <div className="sticky bottom-0"><Player /></div>
     </div>
   );
+}
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
